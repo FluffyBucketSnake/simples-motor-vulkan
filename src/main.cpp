d810a4998a9d8d9f0d1d36358fd031a1afe6c494
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-10-26 15:22:24 -0400

    Código base

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+namespace smv {
+class App {
+  public:
+    void rodar() {}
+  private:
+};
+}  // namespace smv
+
 int main() {
-    std::cout << "Olá mundo!" << std::endl;
-    
-    return 0;
+    smv::App app;
+
+    try {
+        app.rodar();
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
