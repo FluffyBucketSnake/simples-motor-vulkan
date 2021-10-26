d1ca21c0554b4e1911a1e7abcd2c5113cf4d5734
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-10-26 17:49:52 -0400

    Criar instância

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
+#include <vulkan/vulkan.hpp>
+
 namespace smv {
 class App {
   public:
-    void rodar() {}
+    void rodar() {
+        iniciar();
+        destruir();
+    }
+
   private:
+    void iniciar() { criarInstancia(); }
+
+    void criarInstancia() {
+        vk::ApplicationInfo infoApp;
+        infoApp.pApplicationName = "App";
+        infoApp.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        infoApp.pEngineName = "Simples Motor Vulkan";
+        infoApp.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        infoApp.apiVersion = VK_API_VERSION_1_0;
+
+        vk::InstanceCreateInfo info;
+        info.pApplicationInfo = &infoApp;
+        // info.enabledExtensionCount = 0;
+        // info.ppEnabledExtensionNames = nullptr;
+        // info.enabledLayerCount = 0;
+        // info.ppEnabledLayerNames = nullptr;
+
+        instancia_ = vk::createInstance(info);
+    }
+
+    void destruir() { instancia_.destroy(); }
+
+    vk::Instance instancia_;
 };
 }  // namespace smv
 
@@ -13,7 +42,7 @@ int main() {
 
     try {
         app.rodar();
-    } catch(const std::exception& e) {
+    } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
