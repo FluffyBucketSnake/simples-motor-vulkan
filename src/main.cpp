d3ad99affc101a5a3083f96cad81620e53474ada
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-10-26 20:29:10 -0400

    Introduzir camadas de validação

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
 #include <vulkan/vulkan.hpp>
 
@@ -14,6 +16,13 @@ class App {
     void iniciar() { criarInstancia(); }
 
     void criarInstancia() {
+        if (kAtivarCamadasDeValidacao &&
+            !verificarDisponibilidadeDasCamadasDeValidacao()) {
+            throw std::runtime_error(
+                "Esta máquina não suporta as camadas de validação "
+                "necessárias.");
+        }
+
         vk::ApplicationInfo infoApp;
         infoApp.pApplicationName = "App";
         infoApp.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -25,13 +34,46 @@ class App {
         info.pApplicationInfo = &infoApp;
         // info.enabledExtensionCount = 0;
         // info.ppEnabledExtensionNames = nullptr;
-        // info.enabledLayerCount = 0;
-        // info.ppEnabledLayerNames = nullptr;
+        if (kAtivarCamadasDeValidacao) {
+            info.enabledLayerCount =
+                static_cast<uint32_t>(kCamadasDeValidacao.size());
+            info.ppEnabledLayerNames = kCamadasDeValidacao.data();
+        }
 
         instancia_ = vk::createInstance(info);
     }
 
+    bool verificarDisponibilidadeDasCamadasDeValidacao() {
+        auto propriedadesDasCamadas = vk::enumerateInstanceLayerProperties();
+
+        for (auto&& camada : kCamadasDeValidacao) {
+            bool camadaEncontrada = false;
+
+            for (auto&& propriedades : propriedadesDasCamadas) {
+                if (strcmp(camada, propriedades.layerName) == 0) {
+                    camadaEncontrada = true;
+                    break;
+                }
+            }
+
+            if (!camadaEncontrada) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     void destruir() { instancia_.destroy(); }
+
+#ifdef NDEBUG
+    const bool kAtivarCamadasDeValidacao = false;
+#else
+    const bool kAtivarCamadasDeValidacao = true;
+#endif
+
+    const std::vector<const char*> kCamadasDeValidacao = {
+        "VK_LAYER_KHRONOS_validation"};
 
     vk::Instance instancia_;
 };
