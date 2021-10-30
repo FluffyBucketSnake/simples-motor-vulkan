7083ebddba879ddd36edaf84371a3d7c4c2c6aef
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-10-29 21:13:33 -0400

    Add

--- cmake/modules/AddShaderLibrary.cmake
+++ cmake/modules/AddShaderLibrary.cmake
@@ -1,10 +1,9 @@
-# add_shader
-
 find_package(Vulkan COMPONENTS glslc)
 find_program(GLSLC_EXECUTABLE glslc HINTS Vulkan::glslc)
 
 function(add_shader_library TARGET)
-    foreach(SHADER ${ARGN})
+    cmake_parse_arguments(PARSE_ARGV 1 ARG "GLSL;HLSL" "ENV;FORMAT" "SOURCES")
+    foreach(SHADER ${ARG_SOURCES})
         get_filename_component(FILENAME ${SHADER} NAME)
         set(OUTPUT_SHADER ${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}.spv)
         set(COMPILED_SHADERS ${COMPILED_SHADERS} ${OUTPUT_SHADER})
@@ -17,6 +16,7 @@ function(add_shader_library TARGET)
                 ${GLSLC_EXECUTABLE}
                 $<$<BOOL:${ARG_ENV}>:--target-env=${ARG_ENV}>
                 $<$<BOOL:${ARG_FORMAT}>:-mfmt=${ARG_FORMAT}>
+                $<IF:$<BOOL:${ARG_GLSL}>,-xglsl,$<$<BOOL:${ARG_HLSL}>:-xhlsl>>
                 -o ${OUTPUT_SHADER}
                 ${SHADER}
         )
