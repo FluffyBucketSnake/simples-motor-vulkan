a063c00e4552f910846e33e230f088493a4fc89e
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-11-23 20:22:21 -0400

    Incorporar stb_image_write

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #pragma GCC diagnostic ignored "-Wsign-conversion"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 #pragma GCC diagnostic pop
 
 #include <vulkan/vulkan.hpp>
