46020ffc64b889617b0e60f94996e6bb2cab046e
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-11-23 20:41:50 -0400

    Ignorar erro de biblioteca externa

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
