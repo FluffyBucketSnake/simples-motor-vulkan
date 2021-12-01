82cac2a1272e748e3fec0d5ed276421d1000e5dd
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2021-11-30 23:32:07 -0400

    Remover stb_image_write

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,8 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
 #pragma GCC diagnostic pop
 
 #include <vulkan/vulkan.hpp>
