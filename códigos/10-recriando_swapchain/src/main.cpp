cebfaa2e2a950ddd8f1d85ddb67413735ee983a6
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-01-31 22:33:39 -0400

    Remover freeCommandBuffers duplicados dos exemplos 10 e 11

--- códigos/10-recriando_swapchain/src/main.cpp
+++ códigos/10-recriando_swapchain/src/main.cpp
@@ -1194,7 +1194,6 @@ class App {
     }
 
     void destruirContextoDeRenderizacao() {
-        dispositivo_.freeCommandBuffers(poolDeComandos_, buffersDeComandos_);
         for (auto&& framebuffer : framebuffers_) {
             dispositivo_.destroyFramebuffer(framebuffer);
         }
