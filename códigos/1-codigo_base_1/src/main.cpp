3a40106864ca8eb2ace31f2ac531a86ad8f921d0
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-02-26 01:36:26 -0400

    codigos: adicionar "Olá mundo" e "Código base 1"

---
+++ códigos/1-codigo_base_1/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace smv {
+class App {
+  public:
+    void rodar() {}
+
+  private:
+};
+}  // namespace smv
+
+int main() {
+    smv::App app;
+
+    try {
+        app.rodar();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
